eb4ec09d95053bcb61e4e13219dc6c96a7021e61
Charles Kolozsvary <charleskolozsvary@gmail.com>
2025-10-30 18:00:31 -0400

    exact_cover.py updated for hexiamonds

--- exact_cover.py
+++ exact_cover.py
@@ -1,34 +1,29 @@
 '''template from Hooks11 this will be what generates the exact covers in better alignment with the file's name'''
 
-def makeProblemMatrix(grid, polys):
-    gridW = len(grid[0])
-    clues = CLUES[gridW]['polys'] if gridW in CLUES else None
+import polyiamond
+import pickle
+
+def makeProblemMatrix(grid, poly_placements):
     rows = {}
     idx = 0
     keys = set()
     
-    for polyominoname, basePolyomino in polys.items():
-        keys.add(polyominoname)
-        oris = orientations(basePolyomino)
-        placements = getPlacements(polyominoname, oris, grid, clues)
+    for polyname, placements in poly_placements.items():
+        keys.add(polyname)
         for placement in placements:
-            row = {polyominoname: 1}
-            for square in placement:
-                keys.add(square)
-                row[square] = 1
+            placement = placement[1]
+            row = {polyname: 1}
+            for base_shape in placement:
+                keys.add(base_shape)
+                row[base_shape] = 1
             rows[idx] = row
             idx += 1
+
+    assert len(grid) == (len(keys)-len(poly_placements))
+            
     return rows, keys
 
-def withinGrid(gridW, pos):
-    x, y = pos
-    return x >= 0 and x < gridW and y >= 0 and y < gridW
-
-def getCovers(gridW, rows, primaryKeys, prune2x2 = False):
-    
-    polyclues = CLUES[gridW]['polys'] if gridW in CLUES else None
-    polydirs  = CLUES[gridW]['polydirs'] if gridW in CLUES else None
-    
+def getCovers(rows, primaryKeys, maxSolutions = 100):
     pkCounts = {}
     for row in rows.values():
         for key in row:
@@ -43,55 +38,15 @@ def getCovers(gridW, rows, primaryKeys, prune2x2 = False):
         return {v:k for k,v in counts.items()}[min(counts.values())]
     
     solutions = [] #list of lists of row indices
-
-    def existsFilled2x2(crow, curr_filled_squares):
-        filledSquares = curr_filled_squares.copy()
-        for key in crow:
-            if type(key) is not tuple:
-                continue
-            filledSquares[key] = 1
-        for x in range(gridW-1):
-            for y in range(gridW-1):
-                by2 = [(x,y), (x+1,y), (x,y+1), (x+1,y+1)]
-                if all(map(lambda sq: sq in filledSquares, by2)):
-                    return True
-        return False
-
-    def violatesPolyClue(crow, curr_filled_squares):
-        filledSquares = curr_filled_squares.copy()
-        newpoly = None
-        newsquares = []
-        for key in crow:
-            if type(key) is not tuple:
-                newpoly = key
-            elif type(key) is tuple:
-                newsquares.append(key)
-        assert newpoly is not None 
-        curr_polys = set(curr_filled_squares.values())
-        curr_polys.add(newpoly)
-        
-        for sq in newsquares:
-            filledSquares[sq] = newpoly
-
-        for pname, start_pos in polyclues.items():
-            if pname not in curr_polys:
-                continue
-            move = polydirs[start_pos]
-            curr_pos = start_pos            
-            while withinGrid(gridW, curr_pos):
-                if curr_pos in filledSquares:
-                    if filledSquares[curr_pos] == pname:
-                        break
-                    else:
-                        return True
-                        
-                curr_pos = utils.sumTuples(curr_pos, move)
-        return False
             
-    def search(curr_sol, curr_rows, curr_kcounts, curr_filled_squares):
+    def search(curr_sol, curr_rows, curr_kcounts, curr_filled_baseshapes):
         if len(curr_kcounts) == 0:
+            print('SOLUTION FOUND')
             solutions.append(curr_sol)
             return
+
+        if len(solutions) >= maxSolutions:
+            raise Exception ('Done finding solutions')
         
         coverKey = chooseKey(curr_kcounts)
         
@@ -101,17 +56,15 @@ def getCovers(gridW, rows, primaryKeys, prune2x2 = False):
                 candidateRows.append(cridx)
 
         if len(candidateRows) == 0:
-            return
+            return #return unsuccessfully
         
         for chosenRowIdx in candidateRows:
-            chRow = curr_rows[chosenRowIdx] 
-            if prune2x2 and (existsFilled2x2(chRow, curr_filled_squares) or violatesPolyClue(chRow, curr_filled_squares)):
-                continue
+            chRow = curr_rows[chosenRowIdx]
             
             next_sol = curr_sol.copy()
             next_rows = curr_rows.copy()
             next_kcounts = curr_kcounts.copy()
-            next_filled_squares = curr_filled_squares.copy()
+            next_filled_baseshapes = curr_filled_baseshapes.copy()
             
             next_sol.append(chosenRowIdx)            
             for cridx, crow in curr_rows.items():
@@ -135,22 +88,23 @@ def getCovers(gridW, rows, primaryKeys, prune2x2 = False):
 
             ## remove keys that belong to chosen row
             for key in chRow:
-                if type(key) is tuple:
-                    next_filled_squares[key] = chpoly
+                if type(key) is not str:
+                    next_filled_baseshapes[key] = chpoly
                 if key not in primaryKeys:
                     continue
                 assert next_kcounts[key] == 0
                 del next_kcounts[key]
                     
-            search(next_sol, next_rows, next_kcounts, next_filled_squares)
-    
-    search([], rows, pkCounts, {})
+            search(next_sol, next_rows, next_kcounts, next_filled_baseshapes)
 
-    return translateCovers(solutions, rows)
+    try:
+        search([], rows, pkCounts, {})
+    except Exception as message:
+        return translateCovers(solutions, rows)
 
 # The incoming covers are lists of row indices from the problem matrix.
-# The outgoing covers are lists of dictionaries with polyomino names
-# as keys and occupied squares as values
+# The outgoing covers are lists of dictionaries with polyform names
+# as keys and occupied base shapes as values
 def translateCovers(covers_in, matrix):
     covers_out = []
     for cover_in in covers_in:
@@ -158,14 +112,54 @@ def translateCovers(covers_in, matrix):
         rows = [matrix[ridx] for ridx in cover_in]
         for row in rows:
             polyname = None
-            squares = []
+            base_shapes = []
             for key in row:
-                if type(key) is tuple:
-                    squares.append(key)
+                if type(key) is not str:
+                    base_shapes.append(key)
                 else:
                     assert polyname is None, "..."
                     polyname = key
-            cover_out[polyname] = squares
+            cover_out[polyname] = base_shapes
         covers_out.append(cover_out)
     return covers_out
 
+def getPathsForPlacementsInCovers(covers, poly_placements):
+
+    new_covers = []
+    
+    def getPath(polyname, triangles):
+        triangles = frozenset(triangles)
+        placements = poly_placements[polyname]
+        placement_tris = list(map(lambda x: x[1], placements))
+        assert triangles in placement_tris, "hmmm"
+        for path, tris in placements:
+            if tris == triangles:
+                return path
+        assert False, "shouldn't happen"
+        
+    for cover in covers:
+        new_cover = {polyname : [] for polyname in cover}
+        for polyname, triangles in cover.items():
+            path = getPath(polyname, triangles)
+            new_cover[polyname] = (path, triangles)
+        new_covers.append(new_cover)
+    return new_covers
+            
+
+if __name__ == '__main__':
+    grid = polyiamond.makeHexagonishGrid()
+    hexi_p = polyiamond.getPlacements(grid, polyiamond.HEXIAMONDS)
+    matrix, primaryKeys = makeProblemMatrix(grid['triangles'], hexi_p)
+
+    print('len(hexi_p):', len(hexi_p))
+    print("len(grid['triangles']):", len(grid['triangles']))
+    print('len(matrix):', len(matrix))
+    print('len(primaryKeys):', len(primaryKeys))
+    print('len(matrix[0]):', len(matrix[0]))
+
+    covers = getCovers(matrix, primaryKeys)
+
+    new_covers = getPathsForPlacementsInCovers(covers, hexi_p)
+
+    with open('a-few-covers.pkl', 'wb') as f:
+        pickle.dump(new_covers, f)
