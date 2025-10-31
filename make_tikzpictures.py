98792e9c8d390060aa63deb131138f6ea2b3084e
Charles Kolozsvary <charleskolozsvary@gmail.com>
2025-10-30 20:14:37 -0400

    make_tikzpictures.py tidied up

--- make_tikzpictures.py
+++ make_tikzpictures.py
@@ -28,74 +28,71 @@ def rotate30(p):
     return rot_x, rot_y
     
 
-def getPath(eisen_path: list[tuple[int]], make_cycle = True):
+def tikzPath(eisen_path: list[tuple[int]], rotate=True):
     length = len(eisen_path)
     path = ''
     for i, eis in enumerate(eisen_path):
-        x, y = rotate30(eisToCar(eis))
+        x, y = eisToCar(eis)
+        if rotate:
+            x, y = rotate30((x,y))
         path += '({},{})--'.format(x, y)
         if i == length-1:
-            if make_cycle:
-                path += 'cycle'
-            else:
-                path = path[0:-2]
+            path += 'cycle'
     return path
 
 def getTeXpreamble(fileName: str) -> str:
     with open(fileName, 'r') as readFH:
         return ''.join(readFH.readlines())
 
-def orientationsPicture(hname: str, hexiamond: set[tuple[int]]):
+def orientationsTeX(hname: str, hexiamond: set[tuple[int]]):
     oris = polyiamond.orientations(hname, hexiamond)
-    picture = '\\Large\\noindent\\texttt{Rotations:}\n'
+    tex = '\\Large\\noindent\\texttt{Rotations:}\n'
     
-    def drawOris(oris, picture):
-        picture += '\\[\n'
+    def drawOris(oris, tex):
+        tex += '\\[\n'
         for i, ori in enumerate(oris):
-            picture += '\\begin{tikzpicture}\n'
-            path = getPath(ori[0])
-            picture += '\\filldraw[color = {}] {};\n'.format(hname, path)
-            picture += '\\draw[line width = 4pt] {};\n'.format(path)        
-            picture += '\\end{tikzpicture} \ \ \ \n'
+            tex += '\\begin{tikzpicture}\n'
+            path = tikzPath(ori[0], rotate=False)
+            tex += '\\filldraw[color = {}] {};\n'.format(hname, path)
+            tex += '\\draw[line width = 4pt] {};\n'.format(path)        
+            tex += '\\end{tikzpicture} \ \ \ \n'
             if (i+1) % 3 == 0:
-                picture += '\\]\n\n\\['
-        picture += '\\]'
-        return picture
+                tex += '\\]\n\n\\['
+        tex += '\\]'
+        return tex
                 
-    picture = drawOris(oris['rotations'], picture)
-    picture += '\n \\texttt{Mirrors:}\n'
-    picture = drawOris(oris['mirrors'], picture)
+    tex = drawOris(oris['rotations'], tex)
+    tex += '\n \\texttt{Mirrors:}\n'
+    tex = drawOris(oris['mirrors'], tex)
                 
-    return picture
+    return tex
 
 def pdfOrientations(no_extension_fname: str):
     body = ''
     for hname, hexiamond in polyiamond.HEXIAMONDS.items():
-        body += orientationsPicture(hname, hexiamond)
+        body += orientationsTeX(hname, hexiamond)
         body += '\\pagebreak\n\n'
     makePDF(body, no_extension_fname)
 
-def pdfGrid(no_extension_fname: str, eisen_path, points, triangles, point_size = 0.125):
+def tikzGrid(eisen_path, points, triangles, lattice = True, draw_points = False):
+    commands = '\\draw[line width = 3pt] {};\n'.format(tikzPath(eisen_path[0:-1]))
+    point_size = 0.125
+    if draw_points:
+        for p in points:
+            x, y = eisToCar(p)
+            commands += '\\filldraw ({},{}) circle ({});\n'.format(x, y, point_size)
+    if lattice:
+        for tri in triangles:
+            commands += '\\draw {};\n'.format(tikzPath(tri))
+    return commands    
+
+def pdfGrid(no_extension_fname: str, eisen_path, points, triangles):
     body = '\\[\n\\begin{tikzpicture}\n'
-    body += '\\draw[line width = 3pt] {};\n'.format(getPath(eisen_path[0:-1]))
-    # for p in points:
-    #     x, y = eisToCar(p)
-    #     body += '\\filldraw ({},{}) circle ({});\n'.format(x, y, point_size)
-    for tri in triangles:
-        body += '\\draw {};\n'.format(getPath(tri))
+    body += tikzGrid(eisen_path, points, triangles)
     body += '\\end{tikzpicture}\n\\]\n'
     makePDF(body, no_extension_fname)
 
-def tikzGrid(eisen_path, points, triangles, no_lattice = True):
-    commands = '\\draw[line width = 3pt, color = white] {};\n'.format(getPath(eisen_path[0:-1]))
-    commands = '\\draw[line width = 1pt] {};\n'.format(getPath(eisen_path[0:-1]))
-    if no_lattice:
-        return commands
-    for tri in triangles:
-        commands += '\\draw[densely dotted] {};\n'.format(getPath(tri))
-    return commands
-
-def pdfPlacements(no_extension_fname: str):
+def pdfPlacements(no_extension_fname: str, hnames):
     grid = polyiamond.makeHexagonishGrid()
     grid_path, interior_points, grid_triangles = grid['perim'], grid['points'], grid['triangles']
 
@@ -104,95 +101,128 @@ def pdfPlacements(no_extension_fname: str):
     hexi_p = polyiamond.getPlacements(grid, polyiamond.HEXIAMONDS)
     body = ''
     for hname, placements in hexi_p.items():
-        # if hname != 'hexagon':
-        #     continue
+        if hname not in hnames:
+            continue
         for placement in placements:
             body += '\\[\n\\begin{tikzpicture}\n'
             body += tikz_grid            
-            path = getPath(placement[0])
+            path = tikzPath(placement[0])
             body += '\\filldraw[color = {}] {};\n'.format(hname, path)
             body += '\\draw[line width = 3pt] {};\n'.format(path)            
             body += '\\end{tikzpicture}\n\\]\n\\pagebreak\n\n'
-    makePDF(body, no_extension_fname)
+    makePDF(body, 'placements/{}'.format(no_extension_fname), doc_class = 'article')
 
 def pdfHexiamondNames(no_extension_fname: str):
-    body = '\\begin{figure}\n\\captionsetup[subfigure]{labelformat=empty}\n'
+    body = '\\begin{minipage}{28.5pc}\n\\begin{figure}\n\\captionsetup[subfigure]{labelformat=empty}\n'
     i = 1
-    for hname, hex_path in polyiamond.HEXIAMONDS.items():
-            
+    
+    for hname, hex_path in polyiamond.HEXIAMONDS.items():    
         body += '\\begin{subfigure}[b]{4.75pc}\\centering%\\resizebox{!}{7pc}\n{\\begin{tikzpicture}[scale=.525]\n'
-        path = getPath(hex_path)
+        path = tikzPath(hex_path, rotate=False)
         body += '\\filldraw[color = {}] {};\n'.format(hname, path)
         body += '\\draw[line width = 1.5pt] {};\n'.format(path)
         x,y = polyiamond.averageCar(hex_path)
         body += '\\end{tikzpicture}}\n'
         body += '\\caption{{{}}}\n'.format('\large\\texttt{{{}}}'.format(hname))
         body += '\\end{subfigure}'
-        if i % 6 == 0:
+        if i == 6:
             body += '\n\n\\vspace{1ex}\n\n'
         i += 1
-    body += '\\end{figure}\n'
-    makePDF(body, no_extension_fname)
+        
+    body += '\\end{figure}\n\\end{minipage}'
+    makePDF(body, no_extension_fname, doc_class = 'standalone')
 
-def pdfCovers(no_extension_fname, covers):
+def tikzCover(cover, grid_path, blt, wlt, rness):
+    picture = '\\begin{tikzpicture}\n'
+    for polyname, placement in cover.items():
+        hname = re.search('[^-]*', polyname).group(0)
+        path = tikzPath(placement[0])
+        picture += '\\begin{scope}\n'
+        picture += '\\clip[rounded corners={r}pt] {p};\n'.format(p=path,r=rness)            
+        picture += '\\filldraw[color = {n},line cap=butt,rounded corners={r}pt] {p};\n'.format(n=hname, r=rness, p=path)
+        picture += '\\path[draw=black,line width={lw},line cap=butt,rounded corners = {r}pt] {p};\n'.format(lw=blt,r=rness,p=path)    
+        picture += '\\end{scope}\n'
     
+    picture += '\\path[draw=black,line width={lw}pt,line cap=butt,rounded corners={r}pt] {p};\n'.format(lw=blt,r=rness,p=tikzPath(grid_path))
+
+    for polyname, placement in cover.items():
+        hname = re.search('[^-]*', polyname).group(0)
+        path = tikzPath(placement[0])
+        picture += '\\path[draw=white,line cap=butt,line width={lw}pt,rounded corners={r}pt] {p};\n'.format(lw=wlt,r=rness,p=path)                    
+    picture += '\\end{tikzpicture}\n'
+    return picture
+
+def pdfCovers(no_extension_fname, covers, doc_class = 'article'):
     grid = polyiamond.makeHexagonishGrid()
     grid_path = grid['perim']
 
     blt = 2.75 # black line thickness
     wlt = .9   # white line thickness
     rness = 1  # corner roundedness
+
+    is_article = doc_class == 'article'
     
     body = ''
     for cover in covers:
-        body += '\\[\\begin{tikzpicture}\n'
-        for polyname, placement in cover.items():
-            hname = re.search('[^-]*', polyname).group(0)
-            path = getPath(placement[0])
-            body += '\\begin{scope}\n'
-            body += '\\clip[rounded corners={r}pt] {p};\n'.format(p=path,r=rness)            
-            body += '\\filldraw[color = {n},line cap=butt,rounded corners={r}pt] {p};\n'.format(n=hname, r=rness, p=path)
-            body += '\\path[draw=black,line width={lw},line cap=butt,rounded corners = {r}pt] {p};\n'.format(lw=blt,r=rness,p=path)    
-            body += '\\end{scope}\n'
-    
-        body += '\\path[draw=black,line width={lw}pt,line cap=butt,rounded corners={r}pt] {p};\n'.format(lw=blt,r=rness,p=getPath(grid_path))
-
-        for polyname, placement in cover.items():
-            hname = re.search('[^-]*', polyname).group(0)
-            path = getPath(placement[0])
-            body += '\\path[draw=white,line cap=butt,line width={lw}pt,rounded corners={r}pt] {p};\n'.format(lw=wlt,r=rness,p=path)            
+        if is_article:
+            body += '\[\n'
             
-        body += '\\end{tikzpicture}\\]\n\n\\pagebreak\n\n'
-    makePDF(body, no_extension_fname)
+        body += tikzCover(cover, grid_path, blt, wlt, rness)
+        
+        if is_article:
+            body += '\]\n\n\\pagebreak\n\n'
+        
+    makePDF(body, no_extension_fname, doc_class)
     
 
-def makePDF(body: str, no_extension_fname: str):
+def makePDF(body: str, no_extension_fname: str, doc_class = 'article'):
+    cwd = os.getcwd()
+    
     os.chdir('pictures/')
-    tex_file = '{}.tex'.format(no_extension_fname)
+
+    dirname = os.path.dirname(no_extension_fname)
+
+    if dirname != '':
+        os.chdir(dirname)
+
+    basename = os.path.basename(no_extension_fname)
+    
+    tex_file = '{}.tex'.format(basename)
     
     with open(tex_file, 'w') as f:
-        f.write('\\documentclass{article}')
-        f.write(getTeXpreamble('../preamble-tikz.tex'))
+        f.write('\\documentclass{{{cls}}}\n'.format(cls=doc_class))
+        f.write(getTeXpreamble('{}/preamble-tikz.tex'.format(cwd)))
         f.write('\\begin{document}')
         f.write(body)
         f.write('\\end{document}')
         
     os.system('pdflatex {}'.format(tex_file))
-    
-    os.chdir('..')
 
-def oldMain():
-    pdfHexiamondNames('name-hexiamonds')
-    pdfOrientations('hexiamond-orientations')
-    # pdfPlacements('all-placements')
-    
+    os.chdir(cwd)
+
+def oldMain(do_placements = False):
     grid = polyiamond.makeHexagonishGrid()
     grid_path, interior_points, grid_triangles = grid['perim'], grid['points'], grid['triangles']
-    pdfGrid('hexagonish-grid', grid_path, interior_points, grid_triangles)    
     
+    pdfHexiamondNames('name-hexiamonds')
+    pdfOrientations('hexiamond-orientations')
+    pdfGrid('hexagonish-grid', grid_path, interior_points, grid_triangles)
 
-if __name__ == '__main__':
+    if do_placements:
+        for hname in ['hexagon', 'butterfly', 'hook', 'yacht']:
+            pdfPlacements(hname, [hname])
+
+def coverMain(do_all_covers = False):
     with open('a-few-covers.pkl', 'rb') as f:
         covers = pickle.load(f)
-    pdfCovers('one-cover', covers[38:39])
+    
+    if do_all_covers:
+        pdfCovers('100-covers', covers)
+    else:
+        pdfCovers('example-cover', covers[76:77], doc_class = 'standalone')
+        pdfCovers('nice-cover', covers[38:39])
+
+if __name__ == '__main__':
+    oldMain()
+    coverMain()
     
